2033aae36944e59fde685d68c006251455af5a7e
Adetola <adebolaadetola65@gmail.com>
2019-05-30 15:01:32 +0100

    new varargs and date challenges

--- src/App.java
+++ src/App.java
@@ -1,7 +1,68 @@
+import java.io.*;
+import java.lang.reflect.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+
 
 public class App {
-    public static void main(String[] args) {
-        System.out.println(GreatestCommonDivisor1.getGreatestCommonDivisor(18, 9));
+
+    static class Add {
+        static void add (int...numbers){
+            int sum = 0;
+            for (int number: numbers){
+                if (sum != 0){
+                    System.out.print("+");
+                }
+                sum += number;
+                System.out.print(number);
+            }
+            System.out.println("="+sum);
+        }
     }
 
+    public static void main(String[] args) {
+        try{
+            BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+            int n1=Integer.parseInt(br.readLine());
+            int n2=Integer.parseInt(br.readLine());
+            int n3=Integer.parseInt(br.readLine());
+            int n4=Integer.parseInt(br.readLine());
+            int n5=Integer.parseInt(br.readLine());
+            int n6=Integer.parseInt(br.readLine());
+            Add ob=new Add();
+            ob.add(n1,n2);
+            ob.add(n1,n2,n3);
+            ob.add(n1,n2,n3,n4,n5);
+            ob.add(n1,n2,n3,n4,n5,n6);
+            Method[] methods=Add.class.getDeclaredMethods();
+            Set<String> set=new HashSet<>();
+            boolean overload=false;
+            for(int i=0;i<methods.length;i++)
+            {
+                if(set.contains(methods[i].getName()))
+                {
+                    overload=true;
+                    break;
+                }
+                set.add(methods[i].getName());
+
+            }
+            if(overload)
+            {
+                throw new Exception("Overloading not allowed");
+            }
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
 }
+
